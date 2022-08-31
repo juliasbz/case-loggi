76c537cfa9c4d4218f59bd8dbb1161d21f01d150
julia azevedo <juliae@protonmail.com>
2022-08-31 12:16:40 -0300

    código final

---
+++ case-loggi/src/components/DiceStyle.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const DiceImage = styled.img`
+    height: 100px;
+    margin-left: 50px;
+`
+
+export const DiceContainer = styled.div`
+    display: grid;
+    justify-content: center;
+    margin-top: 200px;
+    row-gap: 20px;
+`
+
+export const DiceButton = styled.button`
+	background-color: #ededed;
+	border-radius: 6px;
+	border: 1px solid #dcdcdc;
+	color: black;
+	font-size: 12px;
+	font-weight: bold;
+	padding: 5px;
+`
